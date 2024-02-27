192b4ab98e62343f44dd3eb5747290ff9b93eff4
Yi Ying Li <yiyinl4@andrew.cmu.edu>
2024-02-27 02:48:22 -0500

    add base address

--- src/systick.c
+++ src/systick.c
@@ -13,14 +13,16 @@
 
 #define UNUSED __attribute__((unused))
 
-/** @brief The systick timer register map. */
+/** @brief The systick(STK) timer register map. */
 struct stk_reg_map {
     volatile uint32_t CTRL;      /**<  control and status register (STK_CTRL) */
     volatile uint32_t LOAD;      /**<  SysTick reload value register (STK_LOAD) */
     volatile uint32_t VAL;     /**<  SysTick current value register (STK_VAL) */
     volatile uint32_t CALIB;     /**<  SysTick calibration value register (STK_CALIB) */
-    
 };
+
+/** @brief Base Address of STK */
+#define STK_BASE   (struct stk_reg_map *) 0xE000E010
 
 void systick_init() {
 
